ac93ae725f00c7b72a3505592fb07c5627df0a2e
Øyvind F <oyvind@yvinds-Air.home>
2022-12-24 14:56:22 +0100

    First commit of my az function output binding

---
+++ HttpTrigger11/__init__.py
@@ -0,0 +1,25 @@
+import azure.functions as func
+import json
+import logging
+
+def main(req: func.HttpRequest, inputDocument: func.DocumentList,  outputDocument: func.Out[func.DocumentList]) ->str:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.route_params.get('name')
+    for name in inputDocument:
+        logging.info(name.to_json())
+
+    if name:
+        newdocs = func.DocumentList()
+        logging.info('things are getting there, right about func.documentlist is processed')
+        visitorname = name.to_json()
+        visitor = json.loads(visitorname)
+        logging.info('json is loaded as an dict,')
+        key = "visitor"
+        if key in visitor:
+            visitor[key] +=1
+        newdocs_load_json= json.dumps(visitor)
+        newdocs.append(func.Document.from_json(newdocs_load_json))
+        outputDocument.set(newdocs)
+        
+        return func.HttpResponse(f"Hello, new value is: {newdocs_load_json} . ....")
