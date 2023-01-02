4791d565d4358c16d8ed05efb1e25b51d4cb0092
oyvind3 <71570586+oyvind3@users.noreply.github.com>
2023-01-02 11:33:37 +0000

    Changed some logging info

--- HttpTrigger11/__init__.py
+++ HttpTrigger11/__init__.py
@@ -13,7 +13,7 @@ def main(req: func.HttpRequest, inputDocument: func.DocumentList,  outputDocumen
         logging.info('things are getting there, right about func.documentlist is processed')
         visitorname = name.to_json()
         visitor = json.loads(visitorname)
-        logging.info('json is loaded as an dict,')
+        logging.info('json is loaded')
         key = "visitor"
         if key in visitor:
             visitor[key] +=1
